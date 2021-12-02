e9e32d4a96f9b31692413dc7cd671dae01ad7b0b
unknown <saounmustafa@gmail.com>
2021-12-03 00:32:58 +0500

    withSaveButton

--- App.js
+++ App.js
@@ -8,14 +8,35 @@ import {
   View,
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { Fontisto } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const HistoryIcon = ({ navigation }) => {
+    return (
+      <Fontisto
+        name="history"
+        size={24}
+        color="black"
+        onPress={() => navigation.navigate("HistoryScreen")}
+      />
+    );
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen
+          name="MainScreen"
+          component={MainScreen}
+          options={({ navigation }) => ({
+            title: "Discount Calculator",
+            headerStyle: { backgroundColor: "#F9AA33" },
+            headerTitleAlign: "center",
+            headerRight: () => HistoryIcon({ navigation }),
+          })}
+        />
         <Stack.Screen name="HistoryScreen" component={HistoryScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -26,7 +47,15 @@ const MainScreen = ({ navigation }) => {
   const [discount, setDiscount] = React.useState(null);
   const [saving, setSaving] = React.useState(null);
   const [finalPrice, setFinalPrice] = React.useState(null);
+  //const [disabledBtn, setDisabledBtn] = React.useState(false);
+  const [history, setHistory] = React.useState([]);
 
+  const saveFunc = () => {
+    let historyObj = { op: originalPrice, dis: discount, fp: finalPrice };
+    let newArr = [...history, historyObj];
+    setHistory(newArr);
+    console.log(history);
+  };
   React.useEffect(() => {
     if (originalPrice >= 0) {
       if (discount >= 0 && discount <= 100) {
@@ -39,9 +68,10 @@ const MainScreen = ({ navigation }) => {
       alert("Price cannot be negative!");
     }
   });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.head}>Discount Calculator</Text>
+      <Text style={styles.head}>Let's see how much you save!</Text>
       <TextInput
         style={styles.input}
         placeholder="Enter Price"
@@ -58,11 +88,12 @@ const MainScreen = ({ navigation }) => {
         onChangeText={setDiscount}
         keyboardType="number-pad"
       />
-      <Text style={styles.result}>You Save: {saving}</Text>
-
-      <Text style={styles.result}>Final Price: {finalPrice}</Text>
-      <Pressable onPress={() => navigation.navigate("HistoryScreen")}>
-        <Text>Check History</Text>
+      <View style={styles.resView}>
+        <Text style={styles.result}>You Save: {saving}</Text>
+        <Text style={styles.result}>Final Price: {finalPrice}</Text>
+      </View>
+      <Pressable style={styles.hisBtn} onPress={() => saveFunc()}>
+        <Text>SAVE</Text>
       </Pressable>
     </View>
   );
@@ -93,13 +124,25 @@ const styles = StyleSheet.create({
     borderColor: "#2b2b2b",
   },
   result: {
-    margin: 5,
     color: "#F9AA33",
     fontSize: 22,
   },
+  resView: {
+    marginTop: 20,
+  },
   head: {
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 5,
+    marginBottom: 15,
+  },
+  hisBtn: {
+    marginTop: 25,
+    width: "25%",
+    backgroundColor: "#F9AA33",
+    height: "7%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    borderWidth: 1,
   },
 });
